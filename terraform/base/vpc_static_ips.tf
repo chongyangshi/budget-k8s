bf68176fd01a3025643e62d21d6cf337a824cc67
icydoge <icydoge@gmail.com>
2021-08-15 18:16:42 +0100

    Rename base resources

--- terraform/base/vpc_static_ips.tf
+++ terraform/base/vpc_static_ips.tf
@@ -6,12 +6,3 @@ resource "google_compute_address" "nat_gateway_ip" {
   region       = var.project_region
   project      = var.project_id
 }
-
-// Static IP address attached to the ingress load-balancing instance
-resource "google_compute_address" "ingress_instance_ip" {
-  name = "ingress"
-
-  address_type = "EXTERNAL"
-  region       = var.project_region
-  project      = var.project_id
-}
